29565b09d94c72c4e661e66bb720f29cb8ff5260
Alex <alex@q-83.com>
2022-01-29 14:33:03 +1100

    clean login component

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import clsx from "clsx";
 
 import Footer from "components/Footer/Footer";
 import Nav from "components/Header/Nav";
 import Banner from "components/Header/Banner";
-import clsx from "clsx";
+import about from "assets/about.jpg";
 
 const Login = () => {
   const {
@@ -16,89 +17,95 @@ const Login = () => {
     console.log(data);
     console.log("submit clicked");
   };
-  console.log(errors);
+
+  // left component
+  const LeftComponent = () => {
+    return (
+      <div className="grid md:grid-cols-2 h-full sm:grid-cols-1">
+        <div className="h-full flex items-center justify-center bg-zinc-200/[.5]">
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="bg-white lg:w-96 sm:w-4/5 w-full shadow-lg rounded px-8 pt-6 pb-8 mb-4"
+          >
+            <div class="mb-2">
+              <label class="block text-gray-700 text-sm font-bold mb-2">
+                Username
+              </label>
+              <input
+                defaultValue=""
+                placeholder="email"
+                {...register("email", { required: true })}
+                className={clsx(
+                  "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline",
+                  {
+                    "border-red-500 ": errors.email,
+                  }
+                )}
+              />
+              <div className="h-5">
+                {errors.email && errors.email.type === "required" && (
+                  <span class="text-red-500 block mt-1">Email is required</span>
+                )}
+              </div>
+            </div>
+
+            <div class="mb-2">
+              <label class="block text-gray-700 text-sm font-bold mb-2">
+                Password
+              </label>
+              <input
+                defaultValue=""
+                placeholder="password"
+                {...register("password", { required: true })}
+                className={clsx(
+                  "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline",
+                  {
+                    "border-red-500 ": errors.password,
+                  }
+                )}
+              />
+              <div className="h-5">
+                {errors.password && errors.password.type === "required" && (
+                  <span class="text-red-500 block mt-1">
+                    Password is required
+                  </span>
+                )}
+              </div>
+            </div>
+
+            <div class="flex items-center justify-between">
+              <button
+                type="submit"
+                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              >
+                Sign In
+              </button>
+              <a
+                class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+                href="/"
+              >
+                Forgot Password?
+              </a>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div className="h-screen flex flex-col">
       <div>
         <Banner />
         <Nav />
       </div>
-
-      <div className=" text-gray-700 bg-white flex-1">
-        <div className="grid grid-cols-2 h-full">
-          <div
-            className="h-full flex items-center justify-center"
-            style={{ backgroundColor: "pink" }}
-          >
-            <form
-              onSubmit={handleSubmit(onSubmit)}
-              className="bg-white w-3/5 shadow-md rounded px-8 pt-6 pb-8 mb-4"
-            >
-              <div class="mb-2">
-                <label class="block text-gray-700 text-sm font-bold mb-2">
-                  Username
-                </label>
-                <input
-                  defaultValue=""
-                  placeholder="email"
-                  {...register("email", { required: true })}
-                  className={clsx(
-                    "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline",
-                    {
-                      "border-red-500 ": errors.email,
-                    }
-                  )}
-                />
-                <div className="h-5">
-                  {errors.email && errors.email.type === "required" && (
-                    <span class="text-red-500 block mt-1">
-                      Email is required
-                    </span>
-                  )}
-                </div>
-              </div>
-
-              <div class="mb-2">
-                <label class="block text-gray-700 text-sm font-bold mb-2">
-                  Password
-                </label>
-                <input
-                  defaultValue=""
-                  placeholder="password"
-                  {...register("password", { required: true })}
-                  className={clsx(
-                    "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline",
-                    {
-                      "border-red-500 ": errors.password,
-                    }
-                  )}
-                />
-                <div className="h-5">
-                  {errors.password && errors.password.type === "required" && (
-                    <span class="text-red-500 block mt-1">
-                      Password is required
-                    </span>
-                  )}
-                </div>
-              </div>
-
-              <div class="flex items-center justify-between">
-                <button
-                  type="submit"
-                  class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                >
-                  Sign In
-                </button>
-                <a
-                  class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-                  href="/"
-                >
-                  Forgot Password?
-                </a>
-              </div>
-            </form>
-          </div>
-        </div>
+      <div
+        className="flex-1"
+        style={{
+          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.3)), url(${about})`,
+        }}
+      >
+        <LeftComponent />
       </div>
       <Footer />
     </div>
