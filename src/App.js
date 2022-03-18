b23884496a21b139641a104dc4d3b2477236cfdf
Alex <alex@q-83.com>
2022-03-18 23:18:50 +1100

    reinit react project

--- src/App.js
+++ src/App.js
@@ -1,45 +1,25 @@
-import "index.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Provider } from "react-redux";
+import logo from './logo.svg';
+import './App.css';
 
-import ScrollToTop from "./scrollToTop";
-import store from "./store";
-import Home from "components/Home/Home";
-import Cart from "components/Cart/Cart";
-import About from "components/About/About";
-import Shop from "components/Shop/Shop";
-import Collections from "components/Shop/Collections";
-import ItemDetail from "components/Shop/ItemDetail";
-import Login from "pages/Login/Login";
-import Register from "pages/Register/Register";
-
-const App = () => {
-
-    return (
-        <Provider store={store}>
-            <Router>
-                <ScrollToTop />
-                <Cart />
-                <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route exact path="/about" component={About} />
-                    <Route exact path="/shop" component={Shop} />
-                    <Route
-                        exact
-                        path="/shop/:collection"
-                        component={Collections}
-                    />
-                    <Route
-                        exact
-                        path="/shop/collection/:name"
-                        component={ItemDetail}
-                    />
-                    <Route exact path="/login" component={Login} />
-                    <Route exact path="/register" component={Register} />
-                </Switch>
-            </Router>
-        </Provider>
-    );
-};
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
 export default App;
