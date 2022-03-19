1f0e30357ab23c712e2d609225ba8e979ba325e2
Alex <alex@q-83.com>
2022-03-19 12:16:25 +1100

    测试 pre commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,12 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1
+        className="text-3xl 
+      font-bold underline"
+      >
+        Hello world!
+      </h1>
     </div>
   );
 }
