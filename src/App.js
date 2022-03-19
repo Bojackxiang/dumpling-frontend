f020841341abcdac6aadf8b4ba4adc015eac1ee9
Alex <alex@q-83.com>
2022-03-19 12:17:12 +1100

    re-format

--- src/App.js
+++ src/App.js
@@ -7,12 +7,7 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <h1
-        className="text-3xl 
-      font-bold underline"
-      >
-        Hello world!
-      </h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 }
